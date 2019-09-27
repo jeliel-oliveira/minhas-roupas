eb65395e10130d158f757f022c700045f362e9ee
jeliel <jeliel.augusto10@gmail.com>
2019-09-27 17:46:51 -0400

    Directory e Menu Item, e componentização + estilização da home

--- src/pages/homepage.component.js
+++ src/pages/homepage.component.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss'
+import {Directory} from '../components/directory/directory.component';
 const HomePage = ()=>{
     return (
-        <div className='homePage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>Chapéus</h1>
-                        <span className='subtitle'>Compre agora!</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                    <h1 className='title'>Para Mulheres</h1>
-                        <span className='subtitle'>Compre agora!</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                    <h1 className='title'>Para Homens</h1>
-                        <span className='subtitle'>Compre agora!</span>
-                    </div>
-                </div>
-            </div>
+        <div className='homepage'>
+            <Directory />
         </div>
     );
 }
