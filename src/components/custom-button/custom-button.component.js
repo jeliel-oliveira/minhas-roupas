3bc22315b3adb603232d3b6dfbfbf2a80a30eda6
jeliel <jeliel.augusto10@gmail.com>
2019-10-06 15:03:51 -0400

    funcionalidade do carrinho em redux

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children,isGoogleSignIn,...otherPRops}) => (
-    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in':''} `} 
+const CustomButton = ({children,isGoogleSignIn, inverted ,...otherPRops}) => (
+    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in':''} 
+    ${inverted ? 'inverted':''}`} 
     {...otherPRops}>
         {children}
     </button>
