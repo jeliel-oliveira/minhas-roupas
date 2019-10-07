c238dcca0f0129c0d66e4d32a27284e2b355ffce
jeliel <jeliel.augusto10@gmail.com>
2019-10-07 16:22:06 -0400

    adicionei pagina checkout, checkout item e seletores do reselect

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -5,17 +5,27 @@ import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss';
 import {selectCartItems} from '../../redux/cart/cart.selectors'
 import { createStructuredSelector } from 'reselect';
-const CartDropdown = ({cartItems}) => (
+import {withRouter} from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+                cartItems.length ? cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>) 
+                :
+                <span className='empty-message'>Carrinho Vazio</span>
             }
         </div>
-        <CustomButton>Realizar Pagamento</CustomButton>
+        <CustomButton onClick={()=> {
+                history.push('/checkout')
+                dispatch(toggleCartHidden())
+            }
+        }>
+            Realizar Pagamento
+        </CustomButton>
     </div>
 )
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 })
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -14,3 +14,8 @@ export const selectCartItemsCount =  createSelector(
     [selectCartItems],
     cartItems=>(cartItems.reduce((accQuantity, cartItem) => (accQuantity + cartItem.quantity), 0))
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems=>(cartItems.reduce((accQuantity, cartItem) => (accQuantity + cartItem.quantity * cartItem.price), 0))    
+)
